0b932e4b56611def6c871f32a4a42268b1043a4a
Vandal Paulius Juska <aki.maxis@gmail.com>
2018-01-20 15:50:30 +0000

    Changed payload size, added secret.

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -4,32 +4,31 @@
 #include "nRF24L01.h"
 #include "RF24.h"
 
-int msg[1];
+#define PAYLOAD_SIZE 31
+
 RF24 radio(9,10);
 const uint64_t pipe = 0xE8E8F0F0E1LL;
 int LED1 = 3;
+char secret[] = "77da4ba6-fdf2-11e7-8be5-0ed5f";
 
 void setup(void) {
     Serial.begin(9600);
     radio.begin();
     radio.openReadingPipe(1,pipe);
+    radio.setPayloadSize(PAYLOAD_SIZE);
     radio.startListening();
+    
     pinMode(LED1, OUTPUT);
 }
 
 void loop(void) {
     if (radio.available(pipe)) {
         bool done = false;
+        char message[PAYLOAD_SIZE];
 
         while (!done) {
-            done = radio.read(msg, 1);      
-            Serial.println(msg[0]);
-            if (msg[0] == 111){
-                delay(10);
-                digitalWrite(LED1, HIGH);
-            } else {
-                digitalWrite(LED1, LOW);
-            }
+            done = radio.read(message, PAYLOAD_SIZE);      
+            Serial.println(message);
             delay(10);
         }
     } else {
