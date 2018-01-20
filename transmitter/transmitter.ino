0984600309ddfd1542a5a647e16a6205f520ab49
Vandal Paulius Juska <aki.maxis@gmail.com>
2018-01-20 21:52:56 +0000

    Updated nrf24L01 library files. Fixed radio communication to have low delay. Added DEBUG printfs. Added secret check to receiver.

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,8 +1,12 @@
 // moto-alarm Reciever
+//Used example from http://forum.arduino.cc/index.php?topic=421081
 
 #include  <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
+#include "printf.h"
+
+#define DEBUG
 
 // Push buttons
 #define LOCK        4
@@ -16,7 +20,7 @@
 #define CE          9  //Toggle between transmit (TX), receive (RX), standby, and power-down mode
 #define CSN         10 //SPI chip select 
 
-#define TRANSMIT_DELAY 50  
+#define TRANSMIT_DELAY 140
 #define PAYLOAD_SIZE   31
 
 // Interrupt flags
@@ -26,7 +30,7 @@ volatile bool SENSITIVITY_FLAG = false;
 volatile bool SOUND_FLAG = false;
 
 //Constants
-const char secret[30] = "77da4ba6-fdf2-11e7-8be5-0ed5f";
+const char secret[30] = "77da4ba6-fdf2-11e7-8be5-0ed5ff";
 const uint64_t pipe = 0xE8E8F0F0E1LL;
 const uint8_t lock_code = '0';
 const uint8_t unlock_code = '1';
@@ -36,14 +40,14 @@ const uint8_t sound_code = '3';
 RF24 radio(CE, CSN);
 
 void setup(void) {
-    Serial.begin(115200);
+    #ifdef DEBUG
+        Serial.begin(115200);
+        printf_begin();
+    #endif
 
     initializePins();
+    initializeRadio();
     attachInterrupts();
-
-    radio.begin();
-    radio.setPayloadSize(PAYLOAD_SIZE);
-    radio.openWritingPipe(pipe);
 }
     
 void loop(void) {
@@ -70,31 +74,23 @@ void loop(void) {
     delay(10);
 }
 
-void processKeyPress(uint8_t key_pin, uint8_t key_code){
-    Serial.write(key_code);
-    Serial.println(" pressed");
-    
-    digitalWrite(LED, LOW);
-
-    char msg[PAYLOAD_SIZE];
-    memcpy(msg, secret, sizeof(secret)); 
-    msg[30] = key_code;
-
-    radio.write(msg, sizeof(msg)); 
-    while(!digitalRead(key_pin)){
-        delay(TRANSMIT_DELAY);
-        radio.write(msg, sizeof(msg)); 
-    }
-
-    digitalWrite(LED, HIGH);
-}
-
 void initializePins(){
     pinMode(LOCK, INPUT_PULLUP);
     pinMode(UNLOCK, INPUT_PULLUP);
     pinMode(SENSITIVITY, INPUT_PULLUP);
     pinMode(SOUND, INPUT_PULLUP);
     pinMode(LED, OUTPUT);
+    digitalWrite(LED, HIGH);
+}
+
+void initializeRadio(){
+    radio.begin();
+    radio.setDataRate(RF24_250KBPS);
+    radio.setRetries(3,5); // delay, count
+    radio.setPayloadSize(PAYLOAD_SIZE);
+    radio.setPALevel(RF24_PA_HIGH);
+    radio.openWritingPipe(pipe);
+    radio.stopListening();
 }
 
 void attachInterrupts(){
@@ -102,6 +98,39 @@ void attachInterrupts(){
     pciSetup(UNLOCK);
     pciSetup(SENSITIVITY);
     pciSetup(SOUND);
+}
+
+void processKeyPress(uint8_t key_pin, uint8_t key_code){
+    #ifdef DEBUG   
+        printf("%c pressed\r\n", key_code);
+    #endif
+
+    digitalWrite(LED, LOW);
+
+    char msg[PAYLOAD_SIZE + 1];
+    memcpy(msg, secret, PAYLOAD_SIZE - 1); 
+    msg[PAYLOAD_SIZE - 1] = key_code;
+    msg[PAYLOAD_SIZE] = '\0';
+
+    bool test = radio.write(msg, PAYLOAD_SIZE); 
+
+    #ifdef DEBUG
+        printf("%s %i\r\n", msg, test);
+    #endif
+
+    delay(TRANSMIT_DELAY);
+
+    while(!digitalRead(key_pin)){
+        test = radio.write(msg, PAYLOAD_SIZE); 
+
+        #ifdef DEBUG
+            printf("%s %i\r\n", msg, test);
+        #endif
+        
+        delay(TRANSMIT_DELAY);
+    }
+
+    digitalWrite(LED, HIGH);
 }
 
 void pciSetup(byte pin){
@@ -124,3 +153,4 @@ ISR (PCINT2_vect) {// handle pin change interrupt for D0 to D7 here
         SOUND_FLAG = true;
     }
 }  
+
